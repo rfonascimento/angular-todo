31c203f01e9ef2a738e3c946747bdc3fa8057ca7
nascimento <rfonascimento@gmail.com>
2019-02-09 15:36:53 +0000

    commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,8 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
+import { StoreModule } from '@ngrx/store';
+import { todos } from './redux/reducer'
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -15,6 +17,7 @@ import { TodoComponent } from './todo/todo.component';
     BrowserModule,
     AppRoutingModule,
     FormsModule,
+    StoreModule.forRoot({ todos }),
   ],
   providers: [],
   bootstrap: [AppComponent]
